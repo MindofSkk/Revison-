2d500c72404a000baf99bf47218f6293f88c2e23
MindofSkk <helpmeskk@gmail.com>
2023-01-26 22:30:26 +0530

    important-question

---
+++ Basic Interview Question/JavaScipt.js
@@ -0,0 +1,15 @@
+Clouser
+Promise
+Prototype
+Event loop 
+let var const
+asyncronous and syncronous
+map ,forEach,reduce,filter
+bind , call ,apply
+ES6 Normal Function
+setTimeout setInterval
+fetch
+Callback
+scope(block ,golbal,functional )
+this
+class 
